538b663ec7c6db525071a117238829aed2087926
Zach Riane Machacon <zachriane01@gmail.com>
2023-12-13 10:03:10 +0800

    new: stored currentDate and newsData in localStorage to avoid repetitive api calls

--- context/NewsContext.tsx
+++ context/NewsContext.tsx
@@ -16,25 +16,43 @@ const NewsContext = createContext<NewsContextProps>({
 export const NewsContextProvider = ({ children }: { children: ReactNode }) => {
   const [news, setNews] = useState<Article[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const formattedTodayDate = today.toDateString();
 
   useEffect(() => {
     const updateNews = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(
-          `https://gnews.io/api/v4/search?q=%22crypto%20AND%20bitcoin%22&lang=en&country=us&max=3&apikey=${process.env.NEXT_PUBLIC_GNEWS_API_KEY}`
-        );
-        const mappedArticles: Article[] = response.data.articles.map(
-          (article: any) => ({
-            title: article.title,
-            url: article.url,
-            description: article.description,
-            published_at: new Date(article.publishedAt),
-            source: article.source.name,
-          })
-        );
-        setNews(mappedArticles);
-        setLoading(false);
+        let storedNews = localStorage.getItem("newsData");
+        let currentDate = localStorage.getItem("currentDate");
+        console.log(currentDate);
+        console.log(storedNews);
+        console.log(formattedTodayDate);
+        if (storedNews && currentDate === formattedTodayDate) {
+          setNews(JSON.parse(storedNews));
+          setLoading(false);
+        } else {
+          const response = await axios.get(
+            `https://gnews.io/api/v4/search?q=%22crypto%20AND%20bitcoin%22&lang=en&country=us&max=3&apikey=${process.env.NEXT_PUBLIC_GNEWS_API_KEY}`
+          );
+          const mappedArticles: Article[] = response.data.articles.map(
+            (article: any) => ({
+              title: article.title,
+              url: article.url,
+              description: article.description,
+              published_at: new Date(article.publishedAt),
+              source: article.source.name,
+            })
+          );
+          setNews(mappedArticles);
+          setLoading(false);
+
+          const currentDate = new Date();
+          currentDate.setHours(0, 0, 0, 0);
+          localStorage.setItem("currentDate", currentDate.toDateString());
+          localStorage.setItem("newsData", JSON.stringify(mappedArticles));
+        }
       } catch (error) {
         console.error(error);
       }
@@ -43,10 +61,12 @@ export const NewsContextProvider = ({ children }: { children: ReactNode }) => {
     if (news.length === 0 && loading) {
       updateNews();
     }
-  }, [news]);
+  }, []);
 
   return (
-    <NewsContext.Provider value={{ news, loading }}>{children}</NewsContext.Provider>
+    <NewsContext.Provider value={{ news, loading }}>
+      {children}
+    </NewsContext.Provider>
   );
 };
 
